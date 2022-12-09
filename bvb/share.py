fb1672d6daedf5f79c1a509e39e606e3e995f2f5
Holgyes Orsolya <holgyeso@yahoo.com>
2022-12-09 15:38:43 +0200

    added info dict and symbol, info and company properties

--- bvb/share.py
+++ bvb/share.py
@@ -1,12 +1,9 @@
 from bvb.base import BaseEntity
-
+import re
 
 class Share(BaseEntity):
-    symbol = None
-    isin = None
-    market = None
-    tier = None
-    company = None
+    __symbol = None
+    __info = {}
 
     def __init__(self, symbol: str, params: dict = None, **kwargs):
         super().__init__()
@@ -15,10 +12,56 @@ class Share(BaseEntity):
             # order of merge is important, since explicitly given parameters will be considered in case the params and
             # kwargs both contain the same key
             kwargs = params | kwargs
-        self.isin = kwargs.get("isin")
-        self.market = kwargs.get("market")
-        self.company = kwargs.get("company")
-        self.tier = kwargs.get("tier")
+        self.__info['isin'] = kwargs.get("isin")
+        self.__info['market'] = kwargs.get("market")
+        self.__info['company'] = kwargs.get("company")
+        self.__info['tier'] = kwargs.get("tier")
+    @property
+    def symbol(self):
+        return self.__symbol
+
+    @symbol.setter
+    def symbol(self, symbol: str):
+        # symbols in Romania should contain only alpha chars or numbers
+        if type(symbol) != str:
+            raise TypeError("Symbol should be of type str.")
+        else:
+            symbol = symbol.replace(' ', '')
+            if re.findall("^[a-zA-Z0-9]+$", symbol.replace(' ', '')):
+                self.__symbol = symbol.upper()
+            else:
+                raise ValueError("Share instance cannot be initialized with invalid string")
+
+    @property
+    def info(self):
+        return self.__info
+
+    @info.setter
+    def info(self, info_dict):
+        self.__info = info_dict
+
+    @property
+    def company(self):
+        if 'company' in self.__info:
+            return self.__info['company']
+        else:
+            return None
+
+    @company.setter
+    def company(self, company):
+        self.__info['company'] = company
+    # @property
+    # def company(self):
+    #     return self.__info['company']
+    #
+    # @company.setter
+    # def company(self, company: str):
+    #     if company != '':
+    #         if type(company) != str:
+    #             raise TypeError("Company should be of type str.")
+    #         self.__info['company'] = company
+    #     else:
+    #         raise ValueError("Company should have a value.")
 
     def __repr__(self):
         return f"BVBScraper.Share object <{self.symbol}>"
